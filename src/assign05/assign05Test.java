6c6ec1ef62f9496ff104861091d43bfb067205ad
Zach Sealy <u1371543@utah.edu>
2023-02-20 11:09:37 -0700

    tester class added with J unit testing

---
+++ src/assign05/assign05Test.java
@@ -0,0 +1,12 @@
+package assign05;
+
+import org.junit.jupiter.api.Test;
+
+public class assign05Test {
+
+	@Test
+	public void helloWorld()
+	{
+		assign05.main(null);
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module CS2420_assign05 {
+	requires org.junit.jupiter.api;
 }
